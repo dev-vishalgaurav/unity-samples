3d0e78ea19d0017f80ee35d7d56da9503d96c212
Vishal Gaurav <vishal.gaurav@hotmail.com>
2015-12-01 00:38:12 -0500

    roll a ball game finished tutorial. Need to figure out project structure and commit only important files for the project.
    current ignored .bin files from shadercache.

--- rollaball/Assets/Scripts/PlayerController.cs
+++ rollaball/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-
+    public Text countText;
+    public Text winText;
+    private int count;
     private Rigidbody rb;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
     }
     void Update()
     {
@@ -21,5 +26,27 @@ public class PlayerController : MonoBehaviour {
         float moveVertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
         rb.AddForce(movement*speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count : " + count.ToString();
+        if (count < 12) {
+            winText.text = "";
+        }
+        else
+        {
+            winText.text = "You Win!";
+        }
     }
 }
